621c3a509ef8060c51b68fd0af871be20c37248b
Palak Dhobi <98595690+palak2219@users.noreply.github.com>
2024-04-07 21:20:48 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Credential for admin panel :
+
+Username: admin 
+Password: Test@123
+
+Credential for user panel :
+
+Username: palak@gmail.com
+Password: Test@123
+Who&#8217;s Who
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2022 - CMS Developed by Palak Dhobi
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
